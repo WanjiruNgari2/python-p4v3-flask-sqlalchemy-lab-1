1adca78b4a44c6a68f7a6bed7fe139e99448d927
Damaris Ngari <wanjirungari2@gmail.com>
2025-03-31 03:36:47 +0300

    app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,4 @@
-# server/app.py
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -14,13 +11,38 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+# Route to get an earthquake by ID
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def earthquakes(id):
+    earthquake = db.session.get(Earthquake, id) 
+    if not earthquake:
+        return make_response({'message': f'Earthquake {id} not found.'}, 404)
+    return make_response(earthquake.to_dict(), 200)
+
+# Route to create a new earthquake
+@app.route('/earthquakes', methods=['POST'])
+def create_earthquake():
+    data = request.get_json()
+    earthquake = Earthquake(**data)
+    db.session.add(earthquake)
+    db.session.commit()
+    return make_response(earthquake.to_dict(), 201)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET']) 
+# take a float as a parameter, get all with less than or equal to that magnitude and return the count of matching
+# rows and a list of each row
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(earthquakes)
+
+    return make_response({'count': count, 
+                          'quakes': [e.to_dict() for e in earthquakes]}, 200)
+
 
 
 if __name__ == '__main__':
